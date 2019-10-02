57c09e1cbe85712d790694cbea19d02bee1df2b3
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-10-02 15:27:01 +0200

    Initial commit

---
+++ src/main/java/com/thedigibro/thedigibrojokeapp/ThedigibroJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.thedigibro.thedigibrojokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedigibroJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThedigibroJokeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedigibro/thedigibrojokeapp/ThedigibroJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedigibro.thedigibrojokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThedigibroJokeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
